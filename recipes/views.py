dd9f0e56fc75f65a96012b30eaab1582598b6426
Rafel-Melo <rafael.vilas@gmail.com>
2023-12-01 15:30:47 -0300

    Exemplo de template

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+# HTTP REQUEST
+def home(request):
+    # return HTTP Response
+    return render(request, 'recipes/home.html')
+
+
+def sobre(request):
+    return HttpResponse('SOBRE')
+
+
+def contato(request):
+    return HttpResponse('CONTATO')
+
