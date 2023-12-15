3c4407b6e7e8cbaceb779ae2c3831c7672046668
Rafel-Melo <rafael.vilas@gmail.com>
2023-12-15 17:04:56 -0300

    Usando o shortcut get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse
 from utils.recipes.factory import make_recipe
 from recipes.models import Recipe
@@ -7,18 +7,25 @@ from recipes.models import Recipe
 # HTTP REQUEST
 def home(request):
     # return HTTP Response
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id
-    ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+                'title': f'{recipes[0].category.name} - Category | '
     })
 
 
